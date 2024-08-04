97ae7d7aaee080bd01fac06359e759fb248afeca
Nadika <nadika.name@gmail.com>
2024-08-04 12:33:18 +0100

    add javascript for manipulation menu with keyboard

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const button = document.querySelector('.button');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu__link');
 
 button.addEventListener('click', () => {
     button.classList.toggle('active');
-    button.setAttribute('aria-expended', button.classList.contains('active') ? 'true' : 'false')
+    if (button.classList.contains('active')){
+        button.setAttribute('aria-expended', 'true');
+        menu.setAttribute('aria-hidden', 'false');
+        menuLinks.forEach(link => link.setAttribute('tabindex', '0'));
+    } else{
+        button.setAttribute('aria-expended', 'false');
+        menu.setAttribute('aria-hidden', 'true');
+        menuLinks.forEach(link => link.setAttribute('tabindex', '-1'));
+    }
 }
 )
